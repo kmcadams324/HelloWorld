910f4f7a1a4315ff76d94af04311e7ae2c60bfb7
kmcadams324 <kmcadam1@terpmail.umd.edu>
2024-04-02 16:34:34 -0400

    Added the email to the necessary app routes. Modified the student_view_all.html and student_entry.html to reflect the email present in the new forms as well as show the email for the models.py where I added email to the student table. Lastly, added filler email to create_db.py with the filler 'jdoe123@gmail.com' as well as added it t the for loop at the end

--- app.py
+++ app.py
@@ -47,11 +47,12 @@ def student_create():
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         major_id = request.form['major_id']
+        email = request.form['email']
 
         birth_date = request.form['birth_date']
         is_honors = True if 'is_honors' in request.form else False
 
-        student = Student(first_name=first_name, last_name=last_name, major_id=major_id,
+        student = Student(first_name=first_name, last_name=last_name, major_id=major_id, email=email,
                           birth_date=dt.strptime(birth_date, '%Y-%m-%d'), is_honors=is_honors)
         db.session.add(student)
         db.session.commit()
@@ -83,10 +84,12 @@ def student_edit(student_id):
             student.first_name = request.form['first_name']
             student.last_name = request.form['last_name']
             student.major_id = request.form['major_id']
+            student.email = request.form['email']
             student.birthdate = dt.strptime(request.form['birth_date'], '%Y-%m-%d')
             student.num_credits_completed = request.form['num_credits_completed']
             student.gpa = request.form['gpa']
             student.is_honors = True if 'is_honors' in request.form else False
+
 
             db.session.commit()
             flash(f'{student.first_name} {student.last_name} was successfully updated!', 'success')
